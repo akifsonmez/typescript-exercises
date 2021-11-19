59eb96c649ec3c5f6ae78b568e222e65bd4df238
akifsonmez <akifsonmez@outlook.com>
2021-11-19 23:20:58 +0300

    types added

---
+++ 01_types.js
@@ -0,0 +1,8 @@
+var myName = "Akif";
+var myAge = 28;
+console.log(myName);
+console.log(myAge);
+var twoSum = function (x, y) {
+    return x + y;
+};
+console.log(twoSum(12, 23));
---
+++ 01_types.ts
@@ -0,0 +1,11 @@
+const myName = "Akif"
+const myAge = 28
+
+console.log(myName)
+console.log(myAge)
+
+const twoSum = (x:number, y:number) => {
+    return x + y
+}
+
+console.log(twoSum(12, 23))

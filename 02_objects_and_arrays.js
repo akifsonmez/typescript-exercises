4696f70734f6aac8bca1fc49f68ff2ae767fdf9d
akifsonmez <akifsonmez@outlook.com>
2021-11-20 16:00:43 +0300

    objects and arrays added

---
+++ 02_objects_and_arrays.js
@@ -0,0 +1,17 @@
+var numbers = [12, 1, 3, 4, 56, 89];
+//numbers.push("123") should be number
+var mixed = ["abc", 432];
+//mixed.push(false) should be string or number
+mixed.push("bvd");
+var user = {
+    name: "Akif",
+    age: 25,
+    gender: "male"
+};
+// user.age = "23" should be number
+// user.surname = "Sonmez" new properties cannot be added
+user = {
+    name: "name",
+    age: 12,
+    gender: "male"
+};
---
+++ 02_objects_and_arrays.ts
@@ -0,0 +1,23 @@
+let numbers = [12, 1, 3, 4, 56, 89]
+//numbers.push("123") should be number
+
+let mixed = ["abc", 432]
+//mixed.push(false) should be string or number
+mixed.push("bvd")
+
+
+let user = {
+    name: "Akif",
+    age: 25,
+    gender: "male"
+}
+
+// user.age = "23" should be number
+// user.surname = "Sonmez" new properties cannot be added
+
+
+user = {
+    name: "name",
+    age: 12,
+    gender: "male"
+}

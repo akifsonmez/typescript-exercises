13b68b362dc6ce576484f0c79ab59172e4c071c4
akifsonmez <akifsonmez@outlook.com>
2021-11-22 00:22:20 +0300

    dom element access and type casting added

---
+++ src/07_dom_and_type_casting.ts
@@ -0,0 +1,8 @@
+var anchor = document.querySelector("a")!; // use ! mark if you are certain it exists
+console.log(anchor.href)
+// if(anchor) console.log(anchor.href)
+
+//const form = document.querySelector("form")!
+const form = document.querySelector(".new-item-form") as HTMLFormElement
+
+export {}

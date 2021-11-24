3347e4e54153c22246674108560fae5d350d60e4
akifsonmez <akifsonmez@outlook.com>
2021-11-24 22:17:51 +0300

    invoice class created

--- public/app.js
+++ public/app.js
@@ -1,4 +1,21 @@
 "use strict";
+var Invoice = /** @class */ (function () {
+    function Invoice(client, details, amount) {
+        this.client = client,
+            this.details = details,
+            this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, " owes \u20BA").concat(this.amount, " for ").concat(this.details);
+    };
+    return Invoice;
+}());
+var inv1 = new Invoice("akif", "doing something", 23);
+var inv2 = new Invoice("afif", "doing something more", 238);
+var invoices = [];
+invoices.push(inv1);
+invoices.push(inv2);
+invoices.forEach(function (i) { return console.log(i, i.format()); });
 var form = document.querySelector(".new-item-form");
 var type = document.querySelector("#type");
 var toFrom = document.querySelector("#tofrom");
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,36 @@
+class Invoice {
+    private client: string
+    private details: string
+    private amount: number
+
+    constructor(client: string, details: string, amount: number) {
+        this.client = client,
+            this.details = details,
+            this.amount = amount
+    }
+
+    format() {
+        return `${this.client} owes ₺${this.amount} for ${this.details}`
+    }
+}
+
+const inv1 = new Invoice("akif", "doing something", 23)
+const inv2 = new Invoice("afif", "doing something more", 238)
+
+const invoices: Invoice[] = []
+invoices.push(inv1)
+invoices.push(inv2)
+
+invoices.forEach(i => console.log(i, i.format()))
+
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement
 const type = document.querySelector("#type") as HTMLInputElement
 const toFrom = document.querySelector("#tofrom") as HTMLInputElement
 const details = document.querySelector("#details") as HTMLInputElement
 const amount = document.querySelector("#amount") as HTMLInputElement
 
-form.addEventListener("submit", (e:Event)=>{
+form.addEventListener("submit", (e: Event) => {
     e.preventDefault()
     console.log(
         type.value,
